88861ad729d9973b5938a89050b2efdade48ad28
rchinson <rhinson2010@gmail.com>
2024-10-14 18:20:53 -0700

    finished phase 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,10 @@
 
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
+import SignupFormPage from './components/SignupFormPage';
+import Navigation from './components/Navigation/Navigation.jsx';
 import * as sessionActions from './store/session';
 
 function Layout() {
@@ -18,6 +20,7 @@ function Layout() {
 
   return (
     <>
+      <Navigation isLoaded={isLoaded} />
       {isLoaded && <Outlet />}
     </>
   );
@@ -32,8 +35,12 @@ const router = createBrowserRouter([
         element: <h1>Welcome!</h1>
       },
       {
-        path: '/login',
+        path: "login",
         element: <LoginFormPage />
+      },
+      {
+        path: "signup",
+        element: <SignupFormPage />
       }
     ]
   }
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -16,6 +16,27 @@ if (import.meta.env.MODE === "production") {
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
+
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password
+    })
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+
+
+
 const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
 };
