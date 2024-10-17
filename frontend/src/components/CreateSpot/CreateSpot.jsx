f06c2d90ca9db800a60c623b286be912eabbdd4d
rchinson <rhinson2010@gmail.com>
2024-10-16 18:43:59 -0700

    working on create a spot

---
+++ frontend/src/components/CreateSpot/CreateSpot.jsx
@@ -0,0 +1,126 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+
+import { useDispatch } from 'react-redux';
+
+
+const CreateSpot = () => {
+    const dispatch = useDispatch();
+
+    const [country, setCountry] = useState('');
+    const [streetAddress, setStreetAddress] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [description, setDescription] = useState('');
+    const [title, setTitle] = useState('');
+    const [price, setPrice] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newSpot = {
+            id: nanoid(),
+            country,
+            streetAddress,
+            city,
+            state,
+            description,
+            title,
+            price,
+            imageUrl
+        };
+
+        dispatch(addSpot(newSpot));
+
+        reset();
+    };
+
+    const reset = () => {
+        setCountry('');
+        setStreetAddress('');
+        setCity('');
+        setState('');
+        setDescription('');
+        setTitle('');
+        setPrice('');
+        setImageUrl('');
+    }
+
+
+    return(
+        <div className='spot-input'>
+            <h1>Create a New Spot</h1>
+            <form onSubmit={handleSubmit}>
+
+                <input 
+                    type='text'
+                    onChange={(e) => setCountry(e.target.value)}
+                    value={country}
+                    placeholder='Country'
+                    name='country' 
+                />
+
+                <input 
+                    type='text'
+                    onChange={(e) => setStreetAddress(e.target.value)}
+                    value={streetAddress}
+                    placeholder='Street Address'
+                    name='streetAddress' 
+                />
+
+                <input 
+                    type='text'
+                    onChange={(e) => setCity(e.target.value)}
+                    value={city}
+                    placeholder='City'
+                    name='city' 
+                />
+
+                <input 
+                    type='text'
+                    onChange={(e) => setState(e.target.value)}
+                    value={state}
+                    placeholder='State'
+                    name='state' 
+                />
+
+                <input 
+                    type='text'
+                    onChange={(e) => setDescription(e.target.value)}
+                    value={description}
+                    placeholder='Description'
+                    name='description' 
+                />
+
+                <input 
+                    type='text'
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                    placeholder='Title'
+                    name='title' 
+                />
+
+                <input 
+                    type='text'
+                    onChange={(e) => setPrice(e.target.value)}
+                    value={description}
+                    placeholder= 'Price'
+                    name='price' 
+                />
+
+                <input 
+                    type='text'
+                    onChange={(e) => setImageUrl(e.target.value)}
+                    value={title}
+                    placeholder='imageUrl'
+                    name='imageUrl' 
+                />
+
+
+            </form>
+        </div>
+    )
+
+}
