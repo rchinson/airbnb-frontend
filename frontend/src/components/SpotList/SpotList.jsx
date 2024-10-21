1ac130fb9c1929920eb3c9b633e085ccd123d35a
rchinson <rhinson2010@gmail.com>
2024-10-21 04:19:08 -0700

    removed to fixed

--- frontend/src/components/SpotList/SpotList.jsx
+++ frontend/src/components/SpotList/SpotList.jsx
@@ -46,7 +46,7 @@ function SpotList() {
                             {spot.city}, {spot.state}
                         </p>
                         <p>
-                            <FaStar /> {spot.avgRating ? spot.avgRating.toFixed(1) : "new"}
+                            <FaStar /> {spot.avgRating ? spot.avgRating : "new"}
                         </p>
                     </div>
                         <p>{`$${spot.price} night`}</p>
